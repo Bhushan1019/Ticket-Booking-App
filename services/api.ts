ace41e8b7f1c961719d912971f00843ebdee4b66
Bhushan <121352274+Bhushan1019@users.noreply.github.com>
2025-02-05 17:31:58 +0530

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -2,12 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 import { Platform } from "react-native";
 
-const url = "http://192.168.1.115:8082"; // Use this for both Android and iOS
-
-// const url =
-//   Platform.OS === "android"
-//     ? "http:// 192.168.1.115:8082"
-//     : "http://127.0.0.1:8082";
+const url = "http://IP_ADDRESS:8082"; // enter your device ip address to connect to backend (if using physical device then use your laptops ip address)
 
 const Api: AxiosInstance = axios.create({ baseURL: url + "/api" });
 
